391f90b27641c3cca0778760358d586634fc8482
ttedford <timt@media@ttedfordhome.org>
2017-07-31 17:58:25 -0500

    Adding !price

--- Bitcoin-Bot.py
+++ Bitcoin-Bot.py
@@ -27,21 +27,38 @@ async def btc():
 async def price(currency : str):
     """fetches bitcoin price."""
     currency = currency.upper()
-    url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + currency + '&tsyms=BTC,USD'
-    resp = requests.get(url)
-    rdata = resp.json()
-    if "Response" in rdata:
-        if rdata["Response"] == "Error":
-            return
-    #display = rdata["DISPLAY"]
+    if currency == 'BTC':
+        url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + currency + '&tsyms=USD'
+        resp = requests.get(url)
+        rdata = resp.json()
+        if "Response" in rdata:
+            if rdata["Response"] == "Error":
+                return
+        #display = rdata["DISPLAY"]
 
-    btcprice = rdata["DISPLAY"][currency]['BTC']
-    usdprice = rdata["DISPLAY"][currency]['USD']
+        usdprice = rdata["DISPLAY"][currency]['USD']
 
-    await bot.say("Current price for **" + currency + "**\n"
-                  + 'Last:\t\t**' + str(btcprice['PRICE']) + " (" + str(usdprice['PRICE']) + ")**\n"
-                  + 'Volume (24h):\t**' + str(btcprice['VOLUME24HOURTO']) + "**\n"
-                  + 'Change (24h):\t**' + str(btcprice['CHANGE24HOUR']) + " (" + str(btcprice['CHANGEPCT24HOUR']) + '%)**')
+        await bot.say("Current price for **" + currency + "**\n"
+                      + 'Last:\t\t**' + str(usdprice['PRICE']) + "**\n"
+                      + 'Volume (24h):\t**' + str(usdprice['VOLUME24HOURTO']) + "**\n"
+                      + 'Change (24h):\t**' + str(usdprice['CHANGE24HOUR']) + " (" + str(usdprice['CHANGEPCT24HOUR']) + '%)**')
+        return
+    else:
+        url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + currency + '&tsyms=BTC,USD'
+        resp = requests.get(url)
+        rdata = resp.json()
+        if "Response" in rdata:
+            if rdata["Response"] == "Error":
+                return
+        #display = rdata["DISPLAY"]
+
+        btcprice = rdata["DISPLAY"][currency]['BTC']
+        usdprice = rdata["DISPLAY"][currency]['USD']
+
+        await bot.say("Current price for **" + currency + "**\n"
+                      + 'Last:\t\t**' + str(btcprice['PRICE']) + " (" + str(usdprice['PRICE']) + ")**\n"
+                      + 'Volume (24h):\t**' + str(btcprice['VOLUME24HOURTO']) + "**\n"
+                      + 'Change (24h):\t**' + str(btcprice['CHANGE24HOUR']) + " (" + str(btcprice['CHANGEPCT24HOUR']) + '%)**')
 
 bot.run(BOT_USER_TOKEN)
 
